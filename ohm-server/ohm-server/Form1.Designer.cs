253ef9027538e17ff7ae6a5414d92a7051fe0732
samuderaahr <samuderaahr@gmail.com>
2017-09-15 00:03:49 +0700

    Calling it a day....

---
+++ ohm-server/ohm-server/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ohm_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.intervalTimer = new System.Windows.Forms.Timer(this.components);
+            this.COMPort = new System.Windows.Forms.ComboBox();
+            this.BaudRate = new System.Windows.Forms.ComboBox();
+            this.SendInterval = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.BaudRate = 57600;
+            // 
+            // COMPort
+            // 
+            this.COMPort.FormattingEnabled = true;
+            this.COMPort.Location = new System.Drawing.Point(63, 12);
+            this.COMPort.Name = "COMPort";
+            this.COMPort.Size = new System.Drawing.Size(80, 21);
+            this.COMPort.TabIndex = 0;
+            this.COMPort.DropDown += new System.EventHandler(this.COMPort_DropDown);
+            this.COMPort.SelectedIndexChanged += new System.EventHandler(this.COMPort_SelectedIndexChanged);
+            // 
+            // BaudRate
+            // 
+            this.BaudRate.FormattingEnabled = true;
+            this.BaudRate.Location = new System.Drawing.Point(214, 12);
+            this.BaudRate.Name = "BaudRate";
+            this.BaudRate.Size = new System.Drawing.Size(80, 21);
+            this.BaudRate.TabIndex = 1;
+            this.BaudRate.DropDown += new System.EventHandler(this.BaudRate_DropDown);
+            this.BaudRate.SelectedIndexChanged += new System.EventHandler(this.BaudRate_SelectedIndexChanged);
+            // 
+            // SendInterval
+            // 
+            this.SendInterval.FormattingEnabled = true;
+            this.SendInterval.Location = new System.Drawing.Point(348, 12);
+            this.SendInterval.Name = "SendInterval";
+            this.SendInterval.Size = new System.Drawing.Size(80, 21);
+            this.SendInterval.TabIndex = 2;
+            this.SendInterval.DropDown += new System.EventHandler(this.SendInterval_DropDown);
+            this.SendInterval.SelectedIndexChanged += new System.EventHandler(this.SendInterval_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COM Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Interval";
+            // 
+            // StartButton
+            // 
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(440, 9);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(60, 23);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopButton.Location = new System.Drawing.Point(506, 9);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(60, 23);
+            this.StopButton.TabIndex = 7;
+            this.StopButton.Text = "STOP";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 161);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendInterval);
+            this.Controls.Add(this.BaudRate);
+            this.Controls.Add(this.COMPort);
+            this.Enabled = false;
+            this.Name = "Form1";
+            this.Text = "OHM-Serial v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Timer intervalTimer;
+        private System.Windows.Forms.ComboBox COMPort;
+        private System.Windows.Forms.ComboBox BaudRate;
+        private System.Windows.Forms.ComboBox SendInterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ ohm-server/ohm-server/MainGUI.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ohm_server
+{
+    public partial class Form1 : Form
+    {
+
+        protected StatusBar mainStatusBar = new StatusBar();
+        protected StatusBarPanel statusPanel = new StatusBarPanel();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            statusPanel.BorderStyle = StatusBarPanelBorderStyle.Sunken;
+            statusPanel.Text = "READY";
+            statusPanel.AutoSize = StatusBarPanelAutoSize.Spring;
+            mainStatusBar.Panels.Add(statusPanel);
+
+            mainStatusBar.ShowPanels = true;
+            this.Controls.Add(mainStatusBar);
+
+            serialPort.BaudRate = 9600;
+            intervalTimer.Enabled = true;
+            intervalTimer.Interval = 100;
+
+            COMPort.Enabled = true;
+
+            string[] ports = SerialPort.GetPortNames();
+            int index = 0;
+            Dictionary<Int32, string> COMPortItems = new Dictionary<int, string>();
+
+            foreach (string port in ports)
+            {
+                COMPortItems.Add(index++, port);
+            }
+
+            try
+            {
+                COMPort.DataSource = new BindingSource(COMPortItems, null);
+                COMPort.DisplayMember = "Value";
+                COMPort.ValueMember = "Key";
+
+                COMPort.SelectedIndex = 0;
+            }
+
+            catch (ArgumentNullException) { }
+
+            //Dictionary<string, string> COMPortItems = new Dictionary<string, string>();
+            //COMPortItems.Add("1", "COM3");
+            //COMPortItems.Add("2", "COM4");
+            //COMPortItems.Add("3", "COM5");
+
+            //COMPort.DataSource = new BindingSource(COMPortItems, null);
+            //COMPort.DisplayMember = "Value";
+            //COMPort.ValueMember = "Key";
+
+            //COMPort.SelectedIndex = 1;
+        }
+
+        private void COMPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (COMPort.SelectedIndex)
+            {
+                case 0 : break;
+                case 1 : break;
+                case 2 : break;
+            }
+        }
+
+        private void BaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendInterval_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void COMPort_DropDown(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void BaudRate_DropDown(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void SendInterval_DropDown(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            serialPort.PortName = COMPort.SelectedIndex.ToString();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+          
+        private void Stop()
+        {
+            intervalTimer.Stop();
+            serialPort.Close();
+            
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
